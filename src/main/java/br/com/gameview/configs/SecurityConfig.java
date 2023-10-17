a49917aafd64e41e777d3fd548fb9535f8928e18
DabGias <gabrielfurlaneti@gmail.com>
2023-10-16 21:04:56 -0300

    new: estilo básico

--- src/main/java/br/com/gameview/configs/SecurityConfig.java
+++ src/main/java/br/com/gameview/configs/SecurityConfig.java
@@ -2,7 +2,6 @@ package br.com.gameview.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -11,8 +10,8 @@ public class SecurityConfig {
     @Bean SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
             .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
-            .oauth2Login(Customizer.withDefaults())
-            .formLogin(Customizer.withDefaults())
+            .oauth2Login(login -> login.loginPage("/login").permitAll())
+            .logout(logout -> logout.logoutUrl("/logout").logoutSuccessUrl("/login"))
         .build();
     }
 }
